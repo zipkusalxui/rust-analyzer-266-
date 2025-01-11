6d6707c0a78543a4e0db5356a4a72f1e2fa9c001
higevoltage <izernov19@gmail.com>
2025-01-11 15:08:07 +0300

    New hooks.

--- src/Models/PluginHooksJson.cs
+++ src/Models/PluginHooksJson.cs
@@ -8,10 +8,88 @@ namespace RustAnalyzer.Models
     {
         private const string JsonContent = @"[
             {
+                ""HookName"": ""OnVanishReappear"",
+                ""HookParameters"": [""BasePlayer""],
+                ""PluginName"": ""Vanish""  
+            },
+
+            {
+                ""HookName"": ""OnPlayerMuted"",
+                ""HookParameters"": [""BasePlayer"",""BasePlayer"",""int"",""String""],
+                ""PluginName"": ""IQChat""  
+            },
+
+            {
+                ""HookName"": ""OnModeratorSendBadWords"",
+                ""HookParameters"": [""BasePlayer"",""string""],
+                ""PluginName"": ""IQChat""  
+            },
+
+            {
+                ""HookName"": ""OnPlayerSendBadWords"",
+                ""HookParameters"": [""BasePlayer"",""string""],
+                ""PluginName"": ""IQChat""  
+            },
+
+            {
+                ""HookName"": ""OnHomeRemoved"",
+                ""HookParameters"": [""BasePlayer"",""Vector3"",""string""],
+                ""PluginName"": ""IQTeleportation""  
+            },
+
+            {
+                ""HookName"": ""OnHomeAdded"",
+                ""HookParameters"": [""BasePlayer"",""Vector3"",""string""],
+                ""PluginName"": ""IQTeleportation""  
+            },
+
+            {
+                ""HookName"": ""OnHomeAccepted"",
+                ""HookParameters"": [""BasePlayer"",""string"",""int""],
+                ""PluginName"": ""IQTeleportation""  
+            },
+
+            {
+                ""HookName"": ""OnHomeAccepted"",
+                ""HookParameters"": [""BasePlayer"",""string"",""int""],
+                ""PluginName"": ""IQTeleportation""  
+            },
+
+            {
                 ""HookName"": ""OnPlayerTeleported"",
-                ""HookParameters"": [""BasePlayer"", ""Vector3""],
-                ""PluginName"": ""NTeleportation""
-            }
+                ""HookParameters"": [""BasePlayer"",""Vector3"",""Vector3""],
+                ""PluginName"": ""IQTeleportation""  
+            },
+
+            {
+                ""HookName"": ""OnTeleportAccepted"",
+                ""HookParameters"": [""BasePlayer"",""BasePlayer"",""int""],
+                ""PluginName"": ""IQTeleportation""  
+            },
+
+            {
+                ""HookName"": ""OnTeleportRejected"",
+                ""HookParameters"": [""BasePlayer"",""BasePlayer""],
+                ""PluginName"": ""IQTeleportation""  
+            },
+
+            {
+                ""HookName"": ""CanTeleport"",
+                ""HookParameters"": [""BasePlayer""],
+                ""PluginName"": ""IQTeleportation""  
+            },
+
+            {
+                ""HookName"": ""canTeleport"",
+                ""HookParameters"": [""BasePlayer""],
+                ""PluginName"": ""IQTeleportation""  
+            },
+
+            {
+                ""HookName"": ""OnTeleportRejected"",
+                ""HookParameters"": [""BasePlayer"",""BasePlayer""],
+                ""PluginName"": ""IQTeleportation""  
+            },
         ]";
 
         public static List<PluginHookModel> GetHooks()
