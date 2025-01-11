9b72282953485e1bc38ca5237ed6218543c9d8b2
higevoltage <izernov19@gmail.com>
2025-01-11 18:56:05 +0300

    Again new hooks

--- src/Models/PluginHooksJson.cs
+++ src/Models/PluginHooksJson.cs
@@ -90,6 +90,123 @@ namespace RustAnalyzer.Models
                 ""HookParameters"": [""BasePlayer"",""BasePlayer""],
                 ""PluginName"": ""IQTeleportation""  
             },
+
+            {
+                ""HookName"": ""OnPlayerTeleported"",
+                ""HookParameters"": [""BasePlayer"", ""Vector3"", ""Vector3""],
+                ""PluginName"": ""NTeleportation""
+            },
+            {
+                ""HookName"": ""OnTeleportRejected"",
+                ""HookParameters"": [""BasePlayer"", ""BasePlayer""],
+                ""PluginName"": ""NTeleportation""
+            },
+            {
+                ""HookName"": ""OnTeleportRequestCompleted"",
+                ""HookParameters"": [""BasePlayer"", ""BasePlayer""],
+                ""PluginName"": ""NTeleportation""
+            },
+            {
+                ""HookName"": ""OnTeleportAccepted"",
+                ""HookParameters"": [""BasePlayer"", ""BasePlayer"", ""int""],
+                ""PluginName"": ""NTeleportation""
+            },
+            {
+                ""HookName"": ""OnHomeAccepted"",
+                ""HookParameters"": [""BasePlayer"", ""string"", ""int""],
+                ""PluginName"": ""NTeleportation""
+            },
+            {
+                ""HookName"": ""OnHomeRemoved"",
+                ""HookParameters"": [""BasePlayer"", ""Vector3"", ""string""],
+                ""PluginName"": ""NTeleportation""
+            },
+            {
+                ""HookName"": ""OnHomeAdded"",
+                ""HookParameters"": [""BasePlayer"", ""Vector3"", ""string""],
+                ""PluginName"": ""NTeleportation""
+            },
+            {
+                ""HookName"": ""OnTeleportInterrupted"",
+                ""HookParameters"": [""BasePlayer"", ""string"", ""ulong"", ""string""],
+                ""PluginName"": ""NTeleportation""
+            },
+            {
+                ""HookName"": ""SetPermission"",
+                ""HookParameters"": [""ulong"", ""string"", ""DateTime""],
+                ""PluginName"": ""IQPermissions""
+            },
+            {
+                ""HookName"": ""SetPermission"",
+                ""HookParameters"": [""ulong"", ""string"", ""string""],
+                ""PluginName"": ""IQPermissions""
+            },
+            {
+                ""HookName"": ""SetGroup"",
+                ""HookParameters"": [""ulong"", ""string"", ""DateTime""],
+                ""PluginName"": ""IQPermissions""
+            },
+            {
+                ""HookName"": ""SetGroup"",
+                ""HookParameters"": [""ulong"", ""string"", ""string""],
+                ""PluginName"": ""IQPermissions""
+            },
+            {
+                ""HookName"": ""RevokePermission"",
+                ""HookParameters"": [""ulong"", ""string""],
+                ""PluginName"": ""IQPermissions""
+            },
+            {
+                ""HookName"": ""RevokeGroup"",
+                ""HookParameters"": [""ulong"", ""string"", ""DateTime""],
+                ""PluginName"": ""IQPermissions""
+            },
+            {
+                ""HookName"": ""OnKickPlayer"",
+                ""HookParameters"": [""string"", ""string"", ""BasePlayer""],
+                ""PluginName"": ""IQBanSystem""
+            },
+            {
+                ""HookName"": ""OnBannedPlayerIP"",
+                ""HookParameters"": [""string"", ""string"", ""double"", ""BasePlayer""],
+                ""PluginName"": ""IQBanSystem""
+            },
+            {
+                ""HookName"": ""OnBannedPlayerID"",
+                ""HookParameters"": [""ulong"", ""string"", ""double"", ""BasePlayer""],
+                ""PluginName"": ""IQBanSystem""
+            },
+            {
+                ""HookName"": ""OnUpdateTimeBannedID"",
+                ""HookParameters"": [""string"", ""double"", ""BasePlayer""],
+                ""PluginName"": ""IQBanSystem""
+            },
+            {
+                ""HookName"": ""OnUpdateTimeBannedIP"",
+                ""HookParameters"": [""string"", ""double"", ""BasePlayer""],
+                ""PluginName"": ""IQBanSystem""
+            },
+            {
+                ""HookName"": ""OnChangePermanentBannedID"",
+                ""HookParameters"": [""string"", ""double"", ""BasePlayer""],
+                ""PluginName"": ""IQBanSystem""
+            },
+            {
+                ""HookName"": ""OnChangePermanentBannedIP"",
+                ""HookParameters"": [""string"", ""double"", ""BasePlayer""],
+                ""PluginName"": ""IQBanSystem""
+            },
+            {
+                ""HookName"": ""OnUnbannedID"",
+                ""HookParameters"": [""string"", ""BasePlayer""],
+                ""PluginName"": ""IQBanSystem""
+            },
+            {
+                ""HookName"": ""OnUnbannedIP"",
+                ""HookParameters"": [""string"", ""BasePlayer""],
+                ""PluginName"": ""IQBanSystem""
+            }
+            
         ]";
 
         public static List<PluginHookModel> GetHooks()
